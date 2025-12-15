290a772066dd470084a3ba891ee1344879051bf0
bubobiruk <148684895+bubobiruk@users.noreply.github.com>
2025-12-15 11:18:55 -0400

    Create Logics.dart

---
+++ Logics.dart
@@ -0,0 +1,24 @@
+void main() {
+  var no = "10";
+  if (no == 5) {
+    print("The no i 5");
+  } else if (no == 2) {
+    print("The is 2");
+  } else {
+    print("The no is not 5 or 2, It is $no");
+  }
+}
+/*void main() {
+  var no = 1;
+  for (var i = no; i <= 50; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print("FizzBuzz");
+    } else if (i % 5 == 0) {
+      print("Buzz");
+    } else if (i % 3 == 0) {
+      print("Fizz");
+    } else {
+      print(i);
+    }
+  }
+} */
